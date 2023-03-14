ad6026bc6ad8b762d9ec19fddc321aa37a591ea3
Cyril btvl <cyril.boutteville@hotmail.fr>
2023-03-14 15:43:55 +0100

    modification du composant OwnerPanel pour ajout de fonctionnalités et du composant Winner pour retirer les warning de la console

--- project3/client/src/components/OwnerPanel/index.jsx
+++ project3/client/src/components/OwnerPanel/index.jsx
@@ -1,35 +1,144 @@
-/* eslint-disable no-restricted-globals */
 import { useState, useEffect } from "react";
-import { Segment, Header } from "semantic-ui-react";
+import { Segment, Header, Statistic, Form, Button, Input, Step, Icon, Grid, Divider } from "semantic-ui-react";
 import { useEth } from "../../contexts/EthContext";
 
-function AdminPanel() {
+function OwnerPanel({ currentPhase, setCurrentPhase, phases }) {
   const { state: { accounts, contract, artifact }, } = useEth();
   const [isOwner, setIsOwner] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     async function getOwner() {
       if (artifact) {
-        console.log("AdminPanel : Smartcontract trouvé");
+        console.log("OwnerPanel : user connected");
         // On check si l'account courant est l'owner du contract
         const owner = await contract.methods.owner().call({ from: accounts[0] });
         accounts[0] === owner ? setIsOwner(true) : setIsOwner(false);
       } else {
-        console.log("AdminPanel : Smartcontract non détecté");
-        //alert("PAs de contrat trouvé");
+        console.log("OwnerPanel : user not connected");
+      }
+    }
+
+    async function getPhase() {
+      if (artifact) {
+        // On check si l'account courant est l'owner du contract
+        const phase = await contract.methods.workflowStatus().call({ from: accounts[0] });
+        setCurrentPhase(parseInt(phase));
       }
     }
 
     getOwner();
-  }, [accounts, contract, artifact]);
+    getPhase();
+
+  }, [accounts, contract, artifact, currentPhase, setCurrentPhase]);
+
+  const handleChange = (evt) => {
+    setInputValue(evt.currentTarget.value);
+  };
+
+  const handleSubmit = async () => {
+    if (inputValue === "") {
+      alert("Please enter an address");
+      return;
+    }
+    await contract.methods.addVoter(inputValue).send({ from: accounts[0] });
+    // const newVoter = await contract.methods.getVoter(inputValue).call({ from: accounts[0] });
+    // console.log(newVoter);
+    //location.reload();
+  };
 
   return (
     isOwner && (
       <Segment raised size="huge" color="blue">
-        <Header as="h2">Admin's panel</Header>
-      </Segment>
+        <Header as="h2">Owner's panel</Header>
+        {currentPhase === 0 && (
+          <Form onSubmit={handleSubmit}>
+            <Form.Field>
+              <Input
+                value={inputValue}
+                icon="users"
+                iconPosition="left"
+                placeholder="Add Voter address"
+                size="huge"
+                fluid
+                onChange={handleChange}
+              />
+            </Form.Field>
+
+            <Button icon='add user' content='Add' color="blue" type="submit" fluid />
+          </Form>
+        )}
+        <Divider />
+        <Grid centered columns={2}>
+          <Grid.Column textAlign='center'>
+            <Statistic size='mini' >
+              <Statistic.Label>Voter</Statistic.Label>
+              <Statistic.Value>0</Statistic.Value>
+            </Statistic>
+          </Grid.Column>
+        </Grid >
+        <Segment>
+          <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column width={10}>
+              <Step.Group vertical>
+                <Step completed>
+                  <Icon name='add user' />
+                  <Step.Content>
+                    <Step.Title>Registering Voters</Step.Title>
+                    <Step.Description>Add voters</Step.Description>
+                  </Step.Content>
+                </Step>
+
+                <Step>
+                  <Icon name='edit' />
+                  <Step.Content>
+                    <Step.Title>Proposals Registration Started</Step.Title>
+                    <Step.Description>voters can input proposals</Step.Description>
+                  </Step.Content>
+                </Step>
+
+                <Step>
+                  <Icon name='edit outline' />
+                  <Step.Content>
+                    <Step.Title>Proposals Registration Ended</Step.Title>
+                    <Step.Description>voters can no longer input proposals</Step.Description>
+                  </Step.Content>
+                </Step>
+
+                <Step>
+                  <Icon name='envelope open' />
+                  <Step.Content>
+                    <Step.Title>Voting Session Started</Step.Title>
+                    <Step.Description>voters can vote for one proposal</Step.Description>
+                  </Step.Content>
+                </Step>
+
+                <Step>
+                  <Icon name='envelope outline' />
+                  <Step.Content>
+                    <Step.Title>Voting Session Ended</Step.Title>
+                    <Step.Description>voters can no longer vote</Step.Description>
+                  </Step.Content>
+                </Step>
+
+                <Step>
+                  <Icon name='calculator' />
+                  <Step.Content>
+                    <Step.Title>Votes Tallied</Step.Title>
+                    <Step.Description>Counting of votes</Step.Description>
+                  </Step.Content>
+                </Step>
+              </Step.Group>
+            </Grid.Column>
+            <Grid.Column width={6} verticalAlign='middle'>
+              <Button positive size='huge' content='Next step' />
+            </Grid.Column>
+          </Grid>
+          <Divider vertical><Icon name='angle double right' /></Divider>
+        </Segment>
+      </Segment >
     )
   );
 }
 
-export default AdminPanel;
+export default OwnerPanel;
--- project3/client/src/components/Winner/index.jsx
+++ project3/client/src/components/Winner/index.jsx
@@ -6,20 +6,52 @@ import { useEth } from "../../contexts/EthContext";
 function Winner() {
   const { state: { accounts, contract, artifact }, } = useEth();
   const [proposalWinner, setProposalWinner] = useState([]);
+  const [isVoter, setIsVoter] = useState(false);
 
   useEffect(() => {
-    async function getWinner() {
-      if (contract) {
-        console.log('Winner : contrat exist');
-        const winnerId = await contract.methods.winningProposalID().call({ from: accounts[0] });
-        const winnerProposal = await contract.methods.getOneProposal(parseInt(winnerId)).call({ from: accounts[0] });
-        console.log(winnerProposal);
-        setProposalWinner(winnerProposal);
+    async function getVoterData() {
+      if (artifact) {
+        console.log("VoterPanel : User trouvé");
+
+        try {
+          let allVoterRegistered = await contract.getPastEvents('VoterRegistered', { fromBlock: 0, toBlock: "latest" });
+          let allVoterAddress = allVoterRegistered.map(_ev => _ev.returnValues.voterAddress);
+          let isVoter = allVoterAddress.includes(accounts[0]);
+          console.log("isVoter ? " + isVoter);
+          if (isVoter) {
+            setIsVoter(true);
+            const voterData = await contract.methods.getVoter(accounts[0]).call({ from: accounts[0] });
+            console.log("HasVoter ? " + voterData.hasVoted);
+          } else {
+            console.log("it is not a Voter");
+            setIsVoter(false);
+          }
+
+        } catch (e) {
+          console.log(e)
+        }
       } else {
-        console.log('Winner : contrat does not exist');
+        console.log("VoterPanel : Smartcontract non détecté");
       }
     };
 
+    async function getWinner() {
+      if (isVoter) {
+        if (contract) {
+          console.log('Winner : contrat exist');
+          const winnerId = await contract.methods.winningProposalID().call({ from: accounts[0] });
+          const winnerProposal = await contract.methods.getOneProposal(parseInt(winnerId)).call({ from: accounts[0] });
+          console.log(winnerProposal);
+          setProposalWinner(winnerProposal);
+        } else {
+          console.log('Winner : contrat does not exist');
+        }
+      } else {
+        console.log('Winner : You are not a voter');
+      }
+    };
+
+    getVoterData();
     getWinner();
   }, [accounts, contract, artifact]);
 
